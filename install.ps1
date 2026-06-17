25bc64f18d50b1d6d0d62c18a99394958a19218f
Takashi Tahara <taharatakashi@outlook.com>
2026-06-18 05:59:20 +0900

    chore: minor change

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 # Determine Architecture
 $Arch = if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") { "amd64" } else { "386" }
-$Version = "v1.1.1"
+$Version = "v1.1.2"
 $BinaryName = "blsqui-cli-windows-$Arch.exe"
 $DownloadUrl = "https://github.com/blsqui/blsqui-cli/releases/download/$Version/$BinaryName"
 
