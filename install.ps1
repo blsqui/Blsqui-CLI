a1c645994dfb524264ea8e585e867efc92372885
Takashi Tahara <taharatakashi@outlook.com>
2026-09-19 11:01:12 +0900

    fix: bump internal version constant to v1.1.8

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 # Determine Architecture
 $Arch = if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") { "amd64" } else { "386" }
-$Version = "v1.1.7"
+$Version = "v1.1.8"
 $BinaryName = "blsqui-cli-windows-$Arch.exe"
 $DownloadUrl = "https://github.com/blsqui/blsqui-cli/releases/download/$Version/$BinaryName"
 
